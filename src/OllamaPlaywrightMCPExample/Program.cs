adab41d21ed7875906f1dc6c07c2e43d87b37f2d
Alex@Threadripper <alex@strackers.at>
2025-06-25 19:57:29 +0200

    added MCP example

---
+++ src/OllamaPlaywrightMCPExample/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.AI;
+using ModelContextProtocol.Client;
+using OllamaSharp;
+
+//https://github.com/microsoft/playwright-mcp
+var clientTransport = new StdioClientTransport(new StdioClientTransportOptions
+{
+	Name = "PlaywrightMCP",
+	Command = "npx",
+	Arguments = ["-y", "@playwright/mcp@latest"]
+});
+
+var mcpClient = await McpClientFactory.CreateAsync(clientTransport);
+var tools = await mcpClient.ListToolsAsync();
+ChatOptions chatOptions = new()
+{
+	Tools = tools.ToArray()
+};
+
+string modelId = "qwen3:8b";
+var ollama = new OllamaApiClient(new Uri("http://localhost:11434/"), modelId);
+var chatClient = new ChatClientBuilder(ollama)
+	.UseFunctionInvocation()
+	.Build();
+
+await foreach (var update in chatClient.GetStreamingResponseAsync("open orf.at and show me the response text", chatOptions))
+{
+	Console.Write(update.ToString());
+}
